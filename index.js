38e7e24e4d143766b553860e96dd0aa1980ec0b3
LaercioMBR <laerciombr@gmail.com>
2021-12-27 01:52:44 -0300

    Attempting load jquery on ghpages without messing with CORS

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
+
+
+/* 
+function replaceContent(){
+
+    let reader = new FileReader();
+
+    reader.readAsText("navbar.html");
+    let temp = reader;
+    alert(temp);
+}
+
 function replaceContent(){
     alert("teste string")    
 
 
 }
+
+let buttons = document.getElementsByClassName("nav-link");
+let sections = document.getElementsByClassName("section");
+
+for(let button of buttons){
+    addListenersToDisplay(button)
+}
+
+function addListenersToDisplay(element){
+    let element_id = element.id
+    for(let section of sections){
+        if(section.id.includes(element_id) ){
+            element.addEventListener("click", event => {
+                // alert(section.id)
+                document.getElementById(section.id).style = "display:true";
+            });
+        }
+    }
+}
+
+
+*/
