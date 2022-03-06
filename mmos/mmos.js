c7438a0d7fc01e3d54b47646d249e8594e29137a
LaercioMBR <laerciombr@gmail.com>
2022-03-06 02:24:03 -0300

    Refactoring the monolithic table to something manageable.

--- mmos/mmos.js
+++ mmos/mmos.js
@@ -1,50 +1,33 @@
-function load_mmo_thead(json) {
-    let table_head = document.getElementById('mmo_table_thead_tr')
+function localtime(){
+    let element = this
 
-    for( mmo_metadata in json ){
-        let head = document.createElement("th")
-        head.innerHTML = mmo_metadata
-        table_head.appendChild(head)
-    }
+    alert(this.href)
 }
 
-function load_mmo_tbody(json) {
-    let table_body = document.getElementById('mmo_table_tbody')
-
-    console.log("load mmo tbody JSON")
-    console.log(json)
-    console.log("typeof  JSON")
-    console.log(typeof(json))
-
-    let length = json.length
-    console.log(length)
-    console.log("length of json")
-
-    for(i=0; i<= length; i++ ){
-        let row = document.createElement("tr")
-
-        for(key in json[i]){
-            let tabledata = document.createElement("td")
-            tabledata.innerHTML = json[i][key]
-            row.appendChild(tabledata)
-        }
-        table_body.appendChild(row)
-    }
+function loadTemplate(template_id, data) {
+    var template = document.getElementById(template_id).innerHTML;
+    return eval('`' + template + '`');
 }
 
-function load_table(json){
-    load_mmo_thead(json.meta)
-    load_mmo_tbody(json.data)
+function load_mmo(json) {
+    let data = json.data;
+    
+    let html = "";
+
+    data.forEach(function(value,key){
+        html += loadTemplate("mmo_template", data[key]);
+    });    
+    document.getElementById("content").innerHTML = html;
 }
 
 
-function fetch_mmo_json(){
+function fetch_mmo_json() {
     fetch("../db/mmo.json")
         .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            load_table(data);     
-        });        
+        .then(json => {
+            console.log(json)
+            load_mmo(json);
+        });
 
 }
 
