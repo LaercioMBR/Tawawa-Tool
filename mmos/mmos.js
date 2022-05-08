035490f90af95152ec9a3ac0022874e5c3e26687
LaercioMBR <laerciombr@gmail.com>
2022-05-08 00:56:36 -0300

    dunno, just to keep it updated for now

--- mmos/mmos.js
+++ mmos/mmos.js
@@ -4,7 +4,7 @@ function loadSlider(maxNumberItems){
     
     noUiSlider.create(slider, {
         connect: true,
-        start: [0,1000],
+        start: [0,maxNumberItems],
         range: {
             'min': 0,
             'max': maxNumberItems,
@@ -12,14 +12,12 @@ function loadSlider(maxNumberItems){
         step: 1,
         behaviour: 'tap-drag',
         tooltips: true,
-    
     });
 
 }
 
 function localtime(){
     let currentDate = Date();
-
     alert(currentDate);
 }
 
@@ -33,22 +31,80 @@ function load_mmo(json) {
     
     let html = "";
 
-    loadSlider(json.data.length);
+    let temp = data.length
+
+    let last_mmo = data[temp-1]
+    let last_mmo_number = last_mmo.mmo_number
+
+    loadSlider(Number(last_mmo_number));
     data.forEach(function(value,key){
         html += loadTemplate("mmo_template", data[key]);
-    });    
+        
+        let characterArray = value["character_list"].split(" ");
+
+        for(character of characterArray){
+            character_list.add(character);
+        }
+
+        let contentTagArray = value["general_tag_list"].split(" ");
+
+        for(tag of contentTagArray){
+            content_tag_list.add(tag);
+        }
+
+        
+    });
     document.getElementById("content").innerHTML = html;
 }
 
 
-function fetch_mmo_json() {
-    fetch("../db/mmo.json")
+async function fetch_mmo_json() {
+    await fetch("../db/mmo.json")
         .then(response => response.json())
         .then(json => {
-            console.log(json)
-            load_mmo(json);
+            console.log(json);
+            mmo_json = json;
+            load_mmo(mmo_json);
+            load_modal_characters(character_list);
+            load_modal_content_tag(content_tag_list);
         });
+        
+    }
+    
+function load_modal_characters(character_list){        
+    let element = document.getElementById("modal-table-characters");
 
+    let html = ""
+    character_list.forEach(
+        function(value){
+            html += loadTemplate("table-row-template-content-tag",value);
+        }
+    )
+    
+    element.tbody.innerHTML = html;
 }
 
-fetch_mmo_json()
+
+function load_modal_content_tag(content_tag_list){        
+    let element = document.getElementById("checkbox-content-tag");
+
+    let html = ""
+    content_tag_list.forEach(
+        function(value){
+            html += loadTemplate("checkbox-content-tag-template",value);
+        }
+    )
+    
+    element.innerHTML = html;
+
+    console.log(content_tag_list);
+}
+
+
+let character_list = new Set();
+
+let content_tag_list = new Set();
+
+let mmo_json;
+
+fetch_mmo_json();
